78a56d69f483d55eaff19a6c6215b8bd5d9c75ff
zyrafaq <121723407+zyrafaq@users.noreply.github.com>
2026-09-21 20:17:29 +0200

    Rename game mod to game moderator

--- server/permissions.js
+++ server/permissions.js
@@ -33,7 +33,7 @@ module.exports = [
         class: "menu_betaTester"
     },
     {
-        key: process.env.GAME_MOD,
+        key: process.env.GAME_MODERATOR,
         class: "banHammer"
     },
     {
