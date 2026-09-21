f815532ecc10c2ba35a9f1c5ea33077709d3e847
TGS <93544652+TheGreenSquare@users.noreply.github.com>
2026-09-21 15:56:23 +0000

    add info here

--- server/game/permissions.js
+++ server/game/permissions.js
@@ -9,6 +9,16 @@
 // - 7 // Developer
 
 module.exports = [
+    /* TOKEN PERMISSIONS INFORMATION
+
+        keys            - The ID for the token that goes in `.env`. (Format: "process.env.[TOKEN_NAME]")
+        permissionLevel - Used to determine what commands you have access to.
+
+        class           - The class that `+2 sets you to.
+        nameColor       - The color of your ingame name.
+        spawnAs         - The class you spawn as.
+
+    */
     {
         key: process.env.SHINY,
         class: "menu_shinyMember"
